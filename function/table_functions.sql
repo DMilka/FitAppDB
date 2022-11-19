01b92eb5d5d4d0f20f9a4edb4237d9b65d8c6b81
Dawid Milka <dawid.milka@interia.com>
2022-11-19 09:23:08 +0100

    [feature] Added new sql functions

--- function/table_functions.sql
+++ function/table_functions.sql
@@ -9,3 +9,27 @@ begin
 			return false;
 end;
 ' language plpgsql;
+
+create or replace function execute_constraint_on_table(character varying) returns boolean as '
+declare
+	query alias for $1;
+begin
+	execute query;
+	return true;
+	exception
+		when DUPLICATE_OBJECT then
+			return false;
+end;
+' language plpgsql;
+
+create or replace function execute_operation_on_column_ignore_if_not_exist(character varying) returns boolean as '
+declare
+	query alias for $1;
+begin
+	execute query;
+	return true;
+	exception
+		when UNDEFINED_COLUMN then
+			return false;
+end;
+' language plpgsql;
